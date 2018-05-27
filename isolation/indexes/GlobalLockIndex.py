2d43eff9b724ad5fd29409c59d0d35134d326c0d
Robert Claus <robertclaus@gmail.com>
2018-05-26 22:57:53 -0500

    Different optimization

--- isolation/indexes/GlobalLockIndex.py
+++ isolation/indexes/GlobalLockIndex.py
@@ -1,9 +1,10 @@
+from collections import defaultdict
+
 class GlobalLockIndex:
 
     def __init__(self):
-        self.locking_queries = []
+        self.locking_queries = defaultdict(list)
         self.column_reference = {}
-        self.non_equality_values = 0
         self.equality_index = {}
 
     def set_scheduled_columns(self, column_reference):
@@ -13,15 +14,13 @@ class GlobalLockIndex:
         map(self.add_query,queries)
 
     def add_query(self,query):
-        self.locking_queries.append(query)
-        self.non_equality_values += query.predicatelock.nonequality_value_count
+        self.locking_queries[query.query_id].append(query)
 
     def remove_queries(self, queries):
         map(self.remove_query, queries)
 
     def remove_query(self, query):
-        self.locking_queries.remove(query)
-        self.non_equality_values -= query.predicatelock.nonequality_value_count
+        self.locking_queries[query.query_id].remove(query)
 
     def does_conflict(self, query):
         return any([query.conflicts(existing_query) for existing_query in self.locking_queries])

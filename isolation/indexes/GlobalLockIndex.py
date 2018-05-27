26679cce843665190bbaf22315d9f689f56bed76
Robert Claus <robertclaus@gmail.com>
2018-05-26 23:03:49 -0500

    Try dequeue

--- isolation/indexes/GlobalLockIndex.py
+++ isolation/indexes/GlobalLockIndex.py
@@ -1,10 +1,11 @@
-from collections import defaultdict
+from collections import deque
 
 class GlobalLockIndex:
 
     def __init__(self):
-        self.locking_queries = defaultdict(list)
+        self.locking_queries = deque()
         self.column_reference = {}
+
         self.equality_index = {}
 
     def set_scheduled_columns(self, column_reference):
@@ -14,13 +15,13 @@ class GlobalLockIndex:
         map(self.add_query,queries)
 
     def add_query(self,query):
-        self.locking_queries[query.query_id].append(query)
+        self.locking_queries.append(query)
 
     def remove_queries(self, queries):
         map(self.remove_query, queries)
 
     def remove_query(self, query):
-        self.locking_queries[query.query_id].remove(query)
+        self.locking_queries.remove(query)
 
     def does_conflict(self, query):
         return any([query.conflicts(existing_query) for existing_query in self.locking_queries])

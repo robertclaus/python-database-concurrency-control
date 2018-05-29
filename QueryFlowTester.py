6ec3312e2e0ecb046fa4725c834d69e6289da911
Robert Claus <robertclaus@gmail.com>
2018-05-29 16:37:57 -0500

    Bigger bundle

--- QueryFlowTester.py
+++ QueryFlowTester.py
@@ -46,7 +46,7 @@ class QueryFlowTester:
         # Number of queries to pre-parse so queue does not start empty
         queries_to_start_in_queue_with = min_queries_in_queue
 
-        generator_bundle_size = 10
+        generator_bundle_size = 50
 
         # Load queries to generate.
         query_generator_condition = multiprocessing.Condition()  # Notifies the generator that we may have used some of its queries

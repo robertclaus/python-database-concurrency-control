aa83da5505ee64eaf05f387a1cf0261bf11d1561
Robert Claus <robertclaus@gmail.com>
2018-05-29 18:43:19 -0500

    Reduce volume

--- QueryFlowTester.py
+++ QueryFlowTester.py
@@ -24,10 +24,10 @@ class QueryFlowTester:
         ### Load Settings
 
         # Minimum queries in incoming waiting query queue to allow before generating more
-        min_queries_in_queue = worker_num * 2000
+        min_queries_in_queue = worker_num * 1000
 
         # Minimum queries in sidetrack to consider admitting
-        min_queries_in_sidetrack = 2000
+        min_queries_in_sidetrack = 1000
 
         # Minimum queries to conisder admitting from a sidetrack
         min_queries_from_sidetrack = 100

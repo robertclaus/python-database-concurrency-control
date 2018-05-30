3c783e14b4eddf89e1bb9a361dc330053e0cdd08
Robert Claus <robertclaus@gmail.com>
2018-05-29 20:34:11 -0500

    debug

--- QueryFlowTester.py
+++ QueryFlowTester.py
@@ -24,10 +24,10 @@ class QueryFlowTester:
         ### Load Settings
 
         # Minimum queries in incoming waiting query queue to allow before generating more
-        min_queries_in_queue = worker_num * 2000
+        min_queries_in_queue = worker_num * 3000
 
         # Minimum queries in sidetrack to consider admitting
-        min_queries_in_sidetrack = 2000
+        min_queries_in_sidetrack = 3000
 
         # Minimum queries to consider admitting from a sidetrack
         min_queries_from_sidetrack = 100
@@ -41,7 +41,7 @@ class QueryFlowTester:
         queue_depth = min_queries_in_queue*2  # *10
 
         # How many threads to have generating queries at a time
-        generator_worker_num = worker_num * 6
+        generator_worker_num = worker_num * 5
 
         # Number of queries to pre-parse so queue does not start empty
         queries_to_start_in_queue_with = min_queries_in_queue

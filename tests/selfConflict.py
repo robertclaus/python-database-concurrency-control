91a8a27650c6acfd401edb2bbf5bbd805cbb9ae7
Robert Claus <robertclaus@gmail.com>
2018-06-04 14:11:50 -0500

    Make sure conflicts still work

--- tests/selfConflict.py
+++ tests/selfConflict.py
@@ -6,6 +6,6 @@ query_to_generate = int(sys.argv[2])
 
 q1 = QueryGenerator.QueryGenerator.generate_query(QuerySets.query_sets[query_set], query_to_generate, 1, True)
 q2 = QueryGenerator.QueryGenerator.generate_query(QuerySets.query_sets[query_set], query_to_generate, 1, True)
-columns_to_consider = {"subscriber":["sub_nbr"],"call_forwarding":["s_id"],"special_facility":["s_id"]}
+columns_to_consider = {"subscriber":["sub_nbr"]}
 conflicts= q1.conflicts(q2,columns_to_consider)
 print("Query 1: {}\n\nQuery2: {}\n\nUsing Columns: {}\n\nConflict: {}".format(q1,q2,columns_to_consider,conflicts))

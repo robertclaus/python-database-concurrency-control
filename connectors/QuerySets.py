22cdc1147ee14fb64d9473d4ee97f713bfe40b6a
Robert Claus <robertclaus@gmail.com>
2018-06-04 09:57:41 -0500

    Full TATP

--- connectors/QuerySets.py
+++ connectors/QuerySets.py
@@ -24,7 +24,7 @@ query_sets = [
                subscriber.byte2_6, subscriber.byte2_7, subscriber.byte2_8, subscriber.byte2_9, subscriber.byte2_10, \
                subscriber.msc_location, subscriber.vlr_location \
                FROM subscriber \
-               WHERE subscriber.s_id = <non_uniform_rand_int_subscriber_size>; ",45, # was 35
+               WHERE subscriber.s_id = <non_uniform_rand_int_subscriber_size>; ",35, # was 35
                
                "SELECT call_forwarding.numberx \
                FROM special_facility, call_forwarding \
@@ -40,30 +40,30 @@ query_sets = [
                "SELECT access_info.data1, access_info.data2, access_info.data3, access_info.data4 \
                FROM access_info \
                WHERE access_info.s_id = <non_uniform_rand_int_subscriber_size> \
-               AND access_info.ai_type = <rand_int_1_4>;",45, # was 35
+               AND access_info.ai_type = <rand_int_1_4>;",35, # was 35
                
                "UPDATE subscriber, special_facility \
                SET subscriber.bit_1 = <bit_rand>, special_facility.data_a = <rand_int_1_255> \
                WHERE subscriber.s_id = <non_uniform_rand_int_subscriber_size> \
                AND special_facility.s_id = <non_uniform_rand_int_subscriber_size> \
-               AND special_facility.sf_type = <rand_int_1_4>;", 0, # Was 2
+               AND special_facility.sf_type = <rand_int_1_4>;", 2, # Was 2
                
                "UPDATE subscriber \
                SET subscriber.vlr_location = <rand_int_1_big> \
-               WHERE subscriber.sub_nbr = '<non_uniform_rand_int_subscriber_size_string>';",0, # Was 14
+               WHERE subscriber.sub_nbr = '<non_uniform_rand_int_subscriber_size_string>';",14, # Was 14
                
                "INSERT INTO call_forwarding (call_forwarding.s_id, call_forwarding.sf_type, call_forwarding.start_time, call_forwarding.end_time, call_forwarding.numberx) \
                SELECT subscriber.s_id, special_facility.sf_type ,<rand_0_8_16>,<rand_1_to_24>, <non_uniform_rand_int_subscriber_size> \
                FROM subscriber \
                LEFT OUTER JOIN special_facility ON subscriber.s_id = special_facility.s_id \
                WHERE subscriber.sub_nbr = '<non_uniform_rand_int_subscriber_size_string>' \
-               ORDER BY RAND() LIMIT 1;",0, # Was 2, but self-conflicting was an issue.
+               ORDER BY RAND() LIMIT 1;",2, # Was 2, but self-conflicting was an issue.
                
                "DELETE call_forwarding FROM call_forwarding \
                INNER JOIN subscriber ON subscriber.s_id = call_forwarding.s_id \
                WHERE call_forwarding.sf_type = <rand_int_1_4> \
                AND call_forwarding.start_time = <rand_0_8_16> \
-               AND subscriber.sub_nbr = '<non_uniform_rand_int_subscriber_size_string>';",0, # Was 2
+               AND subscriber.sub_nbr = '<non_uniform_rand_int_subscriber_size_string>';",2, # Was 2
                ],
               
               

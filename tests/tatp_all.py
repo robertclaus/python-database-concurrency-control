26f068db5be4a62c25bf7472e8a95e8ff3252b1b
Robert Claus <robertclaus@gmail.com>
2018-06-08 16:56:19 -0500

    Abstract Connectors!

--- tests/tatp_all.py
+++ tests/tatp_all.py
@@ -5,13 +5,14 @@ from IsolationLevelSetter import IsolationLevelSetter
 from QueryFlowTester import QueryFlowTester
 from clients.MySQLClient import MySQLClient
 from connectors.QueryGeneratorConnector import QueryGeneratorConnector
+from connectors.QuerySets import query_sets
 
 time_to_run = 300
 max_queries = 25000
 
 # Run #1 - Vary Write %
 for query_set in [4]:
-    QueryGeneratorConnector.possible_query_sets = [query_set]
+    QueryGeneratorConnector.possible_query_sets = query_sets[query_set]
     for workers in [2]:#, 4, 6, 8, 10, 12, 14, 16]:
         for isolation_level in ['ru-phased', 'ru-directcomparison', 'ru-zerocc', 'ru', 's']:
             dibs_policy = IsolationLevelSetter.run(isolation_level)

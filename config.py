193510ee6007cd170653d09705ef78afd221055a
Robert Claus <robertclaus@gmail.com>
2018-06-14 17:26:50 -0500

    Did query changes break things?

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ POSTGRES_PASSWORD = 'test'
 POSTGRES_DB_NAME = 't'
 
 # Engine Configuration
-MAX_QUERIES_IN_ENGINE = 10000
+MAX_QUERIES_IN_ENGINE = 30000
 NUMBER_OF_DATABASE_CLIENTS = 4
 MAX_QUERIES_TO_RUN = 10000
 MAX_SECONDS_TO_RUN = 10
@@ -38,6 +38,6 @@ DEFAULT_GENERATOR_WORKER_COUNT = 10
 # Minimum queries to admit for a column key within a phase (will always admit at least once before checking this)
 MIN_QUERIES_TO_ADMIT = 100
 # Maximum queries to run in one phase.  Note MAX_ENGINES_IN_ENGINE may restrict this anyways.
-MAX_QUERIES_PER_PHASE = 10000
+MAX_QUERIES_PER_PHASE = 30000
 # Number of queries to try and admit at once before checking if we should move onto a different phase.
 QUERIES_TO_ADMIT_AT_TIME = 150

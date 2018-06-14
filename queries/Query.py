32b592fa9b478910f9a15fbf44eec434dfd5ce47
Robert Claus <robertclaus@gmail.com>
2018-06-14 17:19:22 -0500

    Did query changes break things?

--- queries/Query.py
+++ queries/Query.py
@@ -7,7 +7,7 @@ import random
 from PredicateLock import PredicateLock
 
 class microQuery:
-    def __init__(self, query_id, query_text, created_at, ps_id):
+    def __init__(self, query_id, query_text, created_at):
         self.id = query_id
         self.query_id = query_id
         self.query_text = query_text
@@ -20,7 +20,7 @@ class microQuery:
         self.completed_at = -1
         self.total_time = -1
         self.waiting_time = -1
-        self.ps_id = ps_id
+
 
 class dbQuery:
     READ = 1
@@ -40,8 +40,13 @@ class dbQuery:
         self.worker = None
         self.worker_waited_time = None
         self.lock_run_under = None
+        self.tables_locked = []
         self.result = None
-        self.lock_indexes = {'columns_locked': []}
+        self.lock_indexes = {'columns_locked': [],
+                             'columns_locked_not_all': [],
+                             'columns_locked_write': [],
+                             'mode': [],
+                             }
         self.error = None
         self.start_admit_time = 0
         self.finish_admit_time = 0
@@ -49,10 +54,9 @@ class dbQuery:
         self.readonly = True
         self.was_admitted = False
         self.completed_at = None
-        self.ps_id = None
 
     def copy_micro(self):
-        return microQuery(self.query_id, self.query_text, self.created_at, self.ps_id)
+        return microQuery(self.query_id, self.query_text, self.created_at)
 
     def merge_micro(self, micro):
         self.result = micro.result
@@ -67,16 +71,52 @@ class dbQuery:
     def __eq__(self, other):
         return self.query_id == other.query_id
 
+    def start_admit(self):
+        self.start_admit_time = time.time()
+
+    def time_since_admit(self):
+        return time.time() - self.start_admit_time
+
+    def finish_admit(self):
+        self.finish_admit_time = time.time()
+        self.time_to_admit = self.finish_admit_time - self.start_admit_time
+        self.was_admitted = True
+
+    def log_error(self, error):
+        self.error = error
+
+    def complete(self):
+        self.completed_at = time.time()
+        self.total_time = self.completed_at - self.created_at
+        self.completed = True
+
+    def done_waiting(self):
+        self.waiting_time = time.time() - self.created_at
+
+    def admitted(self):
+        self.admitted_at = time.time()
+
     def parse(self,skip_read_only=False):
         self.sql_obj = sqlparse.parse(self.query_text)[0]  # Assumes only one query at a time for now
-        Parser.generate_locks(self,skip_read_only)
-        self.sql_obj = None
+        self.generate_locks(skip_read_only)
+        self.strip_fluff()
         self.predicatelock.merge_values()
         self.generate_lock_indexes()
+
+    def strip_fluff(self):
+        self.sql_obj = None
+
+    def compress(self):
+        self.predicatelock = None
+        self.lock_indexes = None
 
     def generate_lock_indexes(self):
         for tabledotcolumn in self.predicatelock.tabledotcolumnindex:
             self.lock_indexes['columns_locked'].append(tabledotcolumn)
+        for tabledotcolumn in self.predicatelock.notalltabledotcolumnindex:
+            self.lock_indexes['columns_locked_not_all'].append(tabledotcolumn)
+        for table in self.predicatelock.tableindex:
+            self.tables_locked.append(table)
         if self.predicatelock.readonly:
             self.readonly = True
         else:
@@ -88,6 +128,117 @@ class dbQuery:
     def get_locked_tables(self):
         return self.predicatelock.tableindex
 
+    def generate_locks(self, skip_read_only):
+        if self.sql_obj.get_type() == 'SELECT':
+            self.readonly = True
+            if skip_read_only:
+                return
+            for token in self.sql_obj.tokens:
+                if type(token) is sqlparse.sql.Where:
+                    for predicate in token:
+                        if type(predicate) is sqlparse.sql.Comparison:
+                            self.add_comparison(predicate, PredicateLock.READ)
+                        if type(predicate) is sqlparse.sql.Parenthesis:
+                            for n_predicate in predicate.tokens:
+                                if type(n_predicate) is sqlparse.sql.Comparison:
+                                    self.add_comparison(n_predicate, PredicateLock.READ)
+
+            for token in self.sql_obj.tokens:
+                if type(token) is sqlparse.sql.Token and token.value == 'SELECT':
+                    in_identifier_list = True
+                    in_table_list = False
+                if type(token) is sqlparse.sql.Token and token.value == 'FROM':
+                    in_identifier_list = False
+                    in_table_list = True
+
+                if in_identifier_list and type(token) is sqlparse.sql.IdentifierList:
+                    for identifier in token:
+                        if type(identifier) is sqlparse.sql.Identifier:
+                            if not self.predicatelock.value_exists(PredicateLock.READ, str(identifier)):
+                                self.add_identifier(identifier, PredicateLock.READ)
+                if in_identifier_list and type(token) is sqlparse.sql.Identifier:
+                    if not self.predicatelock.value_exists(PredicateLock.READ, str(token)):
+                        self.add_identifier(token, PredicateLock.READ)
+
+        if self.sql_obj.get_type() == 'UPDATE':
+            for token in self.sql_obj.tokens:
+                if type(token) is sqlparse.sql.Where:
+                    for predicate in token:
+                        if type(predicate) is sqlparse.sql.Comparison:
+                            self.add_comparison(predicate, PredicateLock.WRITE)
+                        if type(predicate) is sqlparse.sql.Parenthesis:
+                            for n_predicate in predicate.tokens:
+                                if type(n_predicate) is sqlparse.sql.Comparison:
+                                    self.add_comparison(n_predicate, PredicateLock.WRITE)
+
+            for token in self.sql_obj.tokens:
+                if type(token) is sqlparse.sql.Token and token.value == 'UPDATE':
+                    in_table_list = True
+                    in_set_list = False
+                if type(token) is sqlparse.sql.Token and token.value == 'SET':
+                    in_set_list = True
+                    in_table_list = False
+
+                if in_set_list and type(token) is sqlparse.sql.IdentifierList:
+                    for comparison in token:
+                        if type(comparison) is sqlparse.sql.Comparison:
+                            if not self.predicatelock.value_exists(PredicateLock.WRITE, str(comparison[0])):
+                                self.add_identifier(comparison[0], PredicateLock.WRITE)
+                            self.add_comparison(comparison, PredicateLock.WRITE)
+
+                if in_set_list and type(token) is sqlparse.sql.Comparison:
+                    if not self.predicatelock.value_exists(PredicateLock.WRITE, str(token[0])):
+                        self.add_identifier(token[0], PredicateLock.WRITE)
+                    self.add_comparison(token, PredicateLock.WRITE)
+
+        if self.sql_obj.get_type() == "INSERT":
+            for token in self.sql_obj.tokens:
+                if type(token) is sqlparse.sql.Function:
+                    for parens in token.tokens:
+                        if type(parens) is sqlparse.sql.Parenthesis:
+                            for list in parens.tokens:
+                                if type(list) is sqlparse.sql.IdentifierList:
+                                    for id in list.tokens:
+                                        if type(id) is sqlparse.sql.Identifier:
+                                            self.add_identifier(id, PredicateLock.WRITE)
+            for token in self.sql_obj.tokens:
+                if type(token) is sqlparse.sql.Where:
+                    for predicate in token:
+                        if type(predicate) is sqlparse.sql.Comparison:
+                            self.add_comparison(predicate, PredicateLock.WRITE)
+                            if type(predicate) is sqlparse.sql.Parenthesis:
+                                for n_predicate in predicate.tokens:
+                                    if type(n_predicate) is sqlparse.sql.Comparison:
+                                        self.add_comparison(n_predicate, PredicateLock.WRITE)
+
+            for id in ["subscriber.s_id", "special_facility.s_id"]:  # ,"subscriber.sub_nbr"]:
+                self.add_identifier(id, PredicateLock.WRITE)
+
+        if self.sql_obj.get_type() == "DELETE":
+            for token in self.sql_obj.tokens:
+                if type(token) is sqlparse.sql.Comparison:
+                    self.add_comparison(token, PredicateLock.WRITE)
+                if type(token) is sqlparse.sql.Where:
+                    for predicate in token:
+                        if type(predicate) is sqlparse.sql.Comparison:
+                            self.add_comparison(predicate, PredicateLock.WRITE)
+            for id in ["call_forwarding.sf_type", "call_forwarding.numberx"]:
+                self.add_identifier(id, PredicateLock.WRITE)
+
+    def comparison_types(self, comparison_string):
+        if comparison_string == '=':
+            return 1
+        if comparison_string == '>':
+            return 2
+        if comparison_string == '<':
+            return 3
+        if comparison_string == '>=':
+            return 4
+        if comparison_string == '<=':
+            return 5
+        print("Failed to recognize comparison type.")
+        return -1
+
     def add_comparison(self, comparison, mode):
         main_column = str(comparison[0])
         comparison_column = str(comparison[4]).replace("'", "")
@@ -98,7 +249,7 @@ class dbQuery:
             self.add_identifier(comparison_column, mode)
         else:
             self.predicatelock.add_value(main_column,
-                                         Parser.comparison_types(self, str(comparison[2])),
+                                         self.comparison_types(str(comparison[2])),
                                          comparison_column,
                                          mode)
 
@@ -113,117 +264,3 @@ class dbQuery:
 
     def __str__(self):
         return "Query: {}\n{}\nReadonly:{}".format(self.query_text, str(self.predicatelock),self.readonly)
-
-class Parser:
-    @staticmethod
-    def generate_locks(query, skip_read_only):
-        if query.sql_obj.get_type() == 'SELECT':
-            query.readonly = True
-            if skip_read_only:
-                return
-            for token in query.sql_obj.tokens:
-                if type(token) is sqlparse.sql.Where:
-                    for predicate in token:
-                        if type(predicate) is sqlparse.sql.Comparison:
-                            query.add_comparison(predicate, PredicateLock.READ)
-                        if type(predicate) is sqlparse.sql.Parenthesis:
-                            for n_predicate in predicate.tokens:
-                                if type(n_predicate) is sqlparse.sql.Comparison:
-                                    query.add_comparison(n_predicate, PredicateLock.READ)
-
-            for token in query.sql_obj.tokens:
-                if type(token) is sqlparse.sql.Token and token.value == 'SELECT':
-                    in_identifier_list = True
-                    in_table_list = False
-                if type(token) is sqlparse.sql.Token and token.value == 'FROM':
-                    in_identifier_list = False
-                    in_table_list = True
-
-                if in_identifier_list and type(token) is sqlparse.sql.IdentifierList:
-                    for identifier in token:
-                        if type(identifier) is sqlparse.sql.Identifier:
-                            if not query.predicatelock.value_exists(PredicateLock.READ, str(identifier)):
-                                query.add_identifier(identifier, PredicateLock.READ)
-                if in_identifier_list and type(token) is sqlparse.sql.Identifier:
-                    if not query.predicatelock.value_exists(PredicateLock.READ, str(token)):
-                        query.add_identifier(token, PredicateLock.READ)
-
-        if query.sql_obj.get_type() == 'UPDATE':
-            for token in query.sql_obj.tokens:
-                if type(token) is sqlparse.sql.Where:
-                    for predicate in token:
-                        if type(predicate) is sqlparse.sql.Comparison:
-                            query.add_comparison(predicate, PredicateLock.WRITE)
-                        if type(predicate) is sqlparse.sql.Parenthesis:
-                            for n_predicate in predicate.tokens:
-                                if type(n_predicate) is sqlparse.sql.Comparison:
-                                    query.add_comparison(n_predicate, PredicateLock.WRITE)
-
-            for token in query.sql_obj.tokens:
-                if type(token) is sqlparse.sql.Token and token.value == 'UPDATE':
-                    in_table_list = True
-                    in_set_list = False
-                if type(token) is sqlparse.sql.Token and token.value == 'SET':
-                    in_set_list = True
-                    in_table_list = False
-
-                if in_set_list and type(token) is sqlparse.sql.IdentifierList:
-                    for comparison in token:
-                        if type(comparison) is sqlparse.sql.Comparison:
-                            if not query.predicatelock.value_exists(PredicateLock.WRITE, str(comparison[0])):
-                                query.add_identifier(comparison[0], PredicateLock.WRITE)
-                            query.add_comparison(comparison, PredicateLock.WRITE)
-
-                if in_set_list and type(token) is sqlparse.sql.Comparison:
-                    if not query.predicatelock.value_exists(PredicateLock.WRITE, str(token[0])):
-                        query.add_identifier(token[0], PredicateLock.WRITE)
-                    query.add_comparison(token, PredicateLock.WRITE)
-
-        if query.sql_obj.get_type() == "INSERT":
-            for token in query.sql_obj.tokens:
-                if type(token) is sqlparse.sql.Function:
-                    for parens in token.tokens:
-                        if type(parens) is sqlparse.sql.Parenthesis:
-                            for list in parens.tokens:
-                                if type(list) is sqlparse.sql.IdentifierList:
-                                    for id in list.tokens:
-                                        if type(id) is sqlparse.sql.Identifier:
-                                            query.add_identifier(id, PredicateLock.WRITE)
-            for token in query.sql_obj.tokens:
-                if type(token) is sqlparse.sql.Where:
-                    for predicate in token:
-                        if type(predicate) is sqlparse.sql.Comparison:
-                            query.add_comparison(predicate, PredicateLock.WRITE)
-                            if type(predicate) is sqlparse.sql.Parenthesis:
-                                for n_predicate in predicate.tokens:
-                                    if type(n_predicate) is sqlparse.sql.Comparison:
-                                        query.add_comparison(n_predicate, PredicateLock.WRITE)
-
-            for id in ["subscriber.s_id", "special_facility.s_id"]:  # ,"subscriber.sub_nbr"]:
-                query.add_identifier(id, PredicateLock.WRITE)
-
-        if query.sql_obj.get_type() == "DELETE":
-            for token in query.sql_obj.tokens:
-                if type(token) is sqlparse.sql.Comparison:
-                    query.add_comparison(token, PredicateLock.WRITE)
-                if type(token) is sqlparse.sql.Where:
-                    for predicate in token:
-                        if type(predicate) is sqlparse.sql.Comparison:
-                            query.add_comparison(predicate, PredicateLock.WRITE)
-            for id in ["call_forwarding.sf_type", "call_forwarding.numberx"]:
-                query.add_identifier(id, PredicateLock.WRITE)
-
-    @staticmethod
-    def comparison_types(query, comparison_string):
-        if comparison_string == '=':
-            return 1
-        if comparison_string == '>':
-            return 2
-        if comparison_string == '<':
-            return 3
-        if comparison_string == '>=':
-            return 4
-        if comparison_string == '<=':
-            return 5
-        print("Failed to recognize comparison type.")
-        return -1

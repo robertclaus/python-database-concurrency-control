b766c4dc5825c4c73581bc262b6111228a373b06
Robert Claus <robertclaus@gmail.com>
2018-06-14 18:43:02 -0500

    branch figuring this out!!!

--- config.py
+++ config.py
@@ -30,9 +30,9 @@ SUBSCRIBER_COUNT = 5000000
 USE_NON_UNIFORM_RANDOM = True
 
 # Generator
-DEFAULT_TARGET_DEPTH = 3000
+DEFAULT_TARGET_DEPTH = 20000
 GENERATOR_BUNDLE_SIZE = 5
-DEFAULT_GENERATOR_WORKER_COUNT = 10
+DEFAULT_GENERATOR_WORKER_COUNT = 1
 
 # Phased Policy
 # Minimum queries to admit for a column key within a phase (will always admit at least once before checking this)

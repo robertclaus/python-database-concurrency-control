2653cf65e68a20943540e8bffaa08f23db989daa
Robert Claus <robertclaus@gmail.com>
2018-06-14 13:07:22 -0500

    Generator Query size reduction

--- clients/ClientConnectorManager.py
+++ clients/ClientConnectorManager.py
@@ -42,6 +42,7 @@ class ClientConnectorManager:
 
                 try:
                     query.result = connector.execute(query.query_text)
+                    print("Complete query {}".format(query.query_id))
                 except Exception as e:
                     query.error = "ERROR: {}".format(str(e))
 

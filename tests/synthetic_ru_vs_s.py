2080fab8b2fffc998e62555c01eb357a18cc4442
Robert Claus <robertclaus@gmail.com>
2018-06-16 09:53:38 -0500

    Synthetic Tests

--- tests/synthetic_ru_vs_s.py
+++ tests/synthetic_ru_vs_s.py
@@ -16,7 +16,7 @@ config.MAX_QUERIES_IN_ENGINE = 15000
 config.DEFAULT_TARGET_DEPTH = 1000
 config.GENERATOR_BUNDLE_SIZE = 10
 config.DEFAULT_GENERATOR_WORKER_COUNT = 2
-config.MAX_GENERATORS = 10
+config.MAX_GENERATORS = 20
 config.PREGENERATE_ALL_QUERIES = False
 
 # Run #1 - Vary Write %
@@ -26,12 +26,14 @@ for query_set in [Synthetic5050.query_set]:
         config.NUMBER_OF_DATABASE_CLIENTS = workers
         for isolation_level in ['s','ru','s','ru','s','ru']:
             for synthetic_tuples in [10000]:
+                print("Populating DB")
                 dibs_policy = IsolationLevelSetter.run("synthetic-setup")
                 config.MAX_SECONDS_TO_RUN = 1000000
                 config.MAX_QUERIES_TO_RUN = synthetic_tuples
                 QueryGeneratorConnector.possible_query_sets = Insert.query_set
                 DIBSEngine.run(dibs_policy, MySQLClient, QueryGeneratorConnector)
 
+                print("Running Queries")
                 config.MAX_SECONDS_TO_RUN = 20
                 config.MAX_QUERIES_TO_RUN = 1000000
                 dibs_policy = IsolationLevelSetter.run(isolation_level)

d35d967735bf714b50cf74b828ae5f1ec5dec6bb
Robert Claus <robertclaus@gmail.com>
2018-06-15 20:05:53 -0500

    Try different phase approach.

--- config.py
+++ config.py
@@ -43,4 +43,4 @@ MIN_QUERIES_TO_ADMIT = 500
 MAX_QUERIES_PER_PHASE = 15000
 # Number of queries to try and admit at once before checking if we should move onto a different phase.
 QUERIES_TO_ADMIT_AT_TIME = 500
-QUERIES_TO_INITIALLY_ADMIT = 200
+QUERIES_TO_INITIALLY_ADMIT = 500

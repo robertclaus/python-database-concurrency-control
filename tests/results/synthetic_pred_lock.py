ca3f5f10bc1b55dc490f6ef379e13c2de15ccbc7
Robert Claus <robertclaus@gmail.com>
2018-06-16 23:22:08 -0500

    Synthetic Tests

---
+++ tests/results/synthetic_pred_lock.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from tests.IsolationLevelSetter import IsolationLevelSetter
+from DIBSEngine import DIBSEngine
+from clients.MySQLClient import MySQLClient
+from connectors.QueryGeneratorConnector import QueryGeneratorConnector
+from connectors.QuerySets import Synthetic
+
+import config
+
+config.MAX_SECONDS_TO_RUN = 20
+config.MAX_QUERIES_TO_RUN = 1000000
+config.MAX_QUERIES_IN_ENGINE = 15000
+
+config.DEFAULT_TARGET_DEPTH = 1000
+config.GENERATOR_BUNDLE_SIZE = 10
+config.DEFAULT_GENERATOR_WORKER_COUNT = 2
+config.MAX_GENERATORS = 20
+config.PREGENERATE_ALL_QUERIES = False
+
+for readpercent in [10, 20, 30, 40, 50, 60, 70, 80, 90]:
+    QueryGeneratorConnector.possible_query_sets = Synthetic.get_query_set(readpercent)
+    for workers in [20]:
+        config.NUMBER_OF_DATABASE_CLIENTS = workers
+        for isolation_level in ['ru', 'ru-phased', 'ru-directcomparison', 'ru-zerocc', 's']:
+            for synthetic_tuples in [10000]:
+                print("Populating DB")
+                IsolationLevelSetter.setup(synthetic_tuples)
+
+                print("Running Queries")
+                dibs_policy = IsolationLevelSetter.run(isolation_level)
+                QueryGeneratorConnector.last_isolation_level = isolation_level
+                try:
+                    DIBSEngine.run(dibs_policy, MySQLClient, QueryGeneratorConnector)
+                except IOError:
+                    sys.stdout.write("\n\nIO ERROR ENDED TEST\n\n")

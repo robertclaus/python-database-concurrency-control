9ac444301b61ce11d354e3b6ae0e13d85f2e1e80
Robert Claus <robertclaus@gmail.com>
2018-06-26 20:22:23 -0500

    multi-db!

--- clients/PostgresClient.py
+++ clients/PostgresClient.py
@@ -1,3 +1,5 @@
+from psycopg2._psycopg import ProgrammingError
+
 from clients.AbstractClient import AbstractClient
 import config
 
@@ -12,9 +14,16 @@ class PostgresClient(AbstractClient):
         self.cursor = self.connection.cursor()
 
     def execute(self, query_text):
-        self.cursor.execute(query_text)
-        self.connection.commit()
-        return self._result_to_string()
+        try:
+            self.cursor.execute(query_text)
+            self.connection.commit()
+            return self._result_to_string()
+        except ProgrammingError as e:
+            if "no results to fetch" in str(e):
+                return ""
+            else:
+                raise e
+
 
     def _result_to_string(self):
         result = self.cursor.fetchall()
--- tests/results/synthetic_multi_db.py
+++ tests/results/synthetic_multi_db.py
@@ -28,7 +28,7 @@ for readpercent in [10, 30, 50, 70, 90]:
     for workers in [10]:
         config.NUMBER_OF_DATABASE_CLIENTS = workers
         for isolation_level in [ 'ru-directcomparison', 'ru', 'ru-zerocc', 's']:
-            for dbclient in [SqliteClient, PostgresClient, MySQLClient]:
+            for dbclient in [PostgresClient, SqliteClient, MySQLClient]:
                 for synthetic_tuples in [1000]:
                     print("Populating DB")
                     IsolationLevelSetter.setup(synthetic_tuples, dbclient)

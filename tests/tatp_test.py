33ccb10b1023b2f5ff66eb29eb835bc964822e90
Robert Claus <robertclaus@gmail.com>
2018-07-02 17:08:46 -0500

    See if this helps with freezing

--- tests/tatp_test.py
+++ tests/tatp_test.py
@@ -10,7 +10,7 @@ from connectors.QuerySets import TATP
 import config
 
 config.MAX_SECONDS_TO_RUN = 120
-config.MAX_QUERIES_TO_RUN = 25000
+config.MAX_QUERIES_TO_RUN = 30000
 config.MAX_QUERIES_IN_ENGINE = 15000
 config.CLIENT_BUNDLE_SIZE = 1
 
@@ -19,19 +19,19 @@ config.GENERATOR_BUNDLE_SIZE = 10
 config.DEFAULT_GENERATOR_WORKER_COUNT = 2
 config.MAX_GENERATORS = 50
 config.PREGENERATE_ALL_QUERIES = False
-config.QUERIES_TO_NEXT_AT_TIME = 1
+config.QUERIES_TO_NEXT_AT_TIME = 10
 
 # Phased Policy
 # Maximum queries to run in a round of phases. Note MAX_QUERIES_IN_ENGINE may restrict this anyways.
-config.MAX_QUERIES_PER_PHASE = 8000
+config.MAX_QUERIES_PER_PHASE = 10000
 #15000 -> 7500 had little effect.
 
 # Number of queries to try and admit at once before checking if we should move onto a different phase.
 config.QUERIES_TO_ADMIT_AT_TIME = 500
-config.QUERIES_TO_INITIALLY_ADMIT = 36
+config.QUERIES_TO_INITIALLY_ADMIT = 200
 
 # If less than this many queries are in the phase, we will move on to the next phase (will always admit at least once before checking this)
-config.MIN_QUERIES_TO_ADMIT = 35
+config.MIN_QUERIES_TO_ADMIT = 500
 
 for dbclient in [MySQLClient]:
     QueryGeneratorConnector.possible_query_sets = TATP.query_set

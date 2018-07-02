59d645057d112bba3b0af5626f89fa201cd02014
Robert Claus <robertclaus@gmail.com>
2018-07-02 17:14:24 -0500

    trying to optimize for tatp

--- tests/tatp_test.py
+++ tests/tatp_test.py
@@ -19,7 +19,7 @@ config.GENERATOR_BUNDLE_SIZE = 10
 config.DEFAULT_GENERATOR_WORKER_COUNT = 2
 config.MAX_GENERATORS = 50
 config.PREGENERATE_ALL_QUERIES = False
-config.QUERIES_TO_NEXT_AT_TIME = 10
+config.QUERIES_TO_NEXT_AT_TIME = 20
 
 # Phased Policy
 # Maximum queries to run in a round of phases. Note MAX_QUERIES_IN_ENGINE may restrict this anyways.
@@ -28,7 +28,7 @@ config.MAX_QUERIES_PER_PHASE = 10000
 
 # Number of queries to try and admit at once before checking if we should move onto a different phase.
 config.QUERIES_TO_ADMIT_AT_TIME = 500
-config.QUERIES_TO_INITIALLY_ADMIT = 200
+config.QUERIES_TO_INITIALLY_ADMIT = 300
 
 # If less than this many queries are in the phase, we will move on to the next phase (will always admit at least once before checking this)
 config.MIN_QUERIES_TO_ADMIT = 500
@@ -37,7 +37,7 @@ for dbclient in [MySQLClient]:
     QueryGeneratorConnector.possible_query_sets = TATP.query_set
     for workers in [12]:
         config.NUMBER_OF_DATABASE_CLIENTS = workers
-        for isolation_level in ['ru-phased','ru', 's']:
+        for isolation_level in ['ru-phased','s','ru','s']:
             dibs_policy = IsolationLevelSetter.run(isolation_level,dbclient)
 
             try:

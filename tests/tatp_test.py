2f7810c90bed3efbee08dfa59d4ae5621b2eb7ed
Robert Claus <robertclaus@gmail.com>
2018-07-02 14:11:30 -0500

    tatp retest high performance

--- tests/tatp_test.py
+++ tests/tatp_test.py
@@ -11,26 +11,26 @@ import config
 
 config.MAX_SECONDS_TO_RUN = 120
 config.MAX_QUERIES_TO_RUN = 25000
-config.MAX_QUERIES_IN_ENGINE = 100
+config.MAX_QUERIES_IN_ENGINE = 1000
 config.CLIENT_BUNDLE_SIZE = 3
 
-config.DEFAULT_TARGET_DEPTH = 100
-config.GENERATOR_BUNDLE_SIZE = 1
+config.DEFAULT_TARGET_DEPTH = 1000
+config.GENERATOR_BUNDLE_SIZE = 8
 config.DEFAULT_GENERATOR_WORKER_COUNT = 2
 config.MAX_GENERATORS = 25
 config.PREGENERATE_ALL_QUERIES = False
-config.QUERIES_TO_NEXT_AT_TIME = 10
+config.QUERIES_TO_NEXT_AT_TIME = 24
 
 # Phased Policy
 # Maximum queries to run in a round of phases. Note MAX_QUERIES_IN_ENGINE may restrict this anyways.
 config.MAX_QUERIES_PER_PHASE = 15000
 
 # Number of queries to try and admit at once before checking if we should move onto a different phase.
-config.QUERIES_TO_ADMIT_AT_TIME = 1
-config.QUERIES_TO_INITIALLY_ADMIT = 1
+config.QUERIES_TO_ADMIT_AT_TIME = 500
+config.QUERIES_TO_INITIALLY_ADMIT = 200
 
 # If less than this many queries are in the phase, we will move on to the next phase (will always admit at least once before checking this)
-config.MIN_QUERIES_TO_ADMIT = 1
+config.MIN_QUERIES_TO_ADMIT = 500
 
 for dbclient in [MySQLClient]:
     QueryGeneratorConnector.possible_query_sets = TATP.query_set

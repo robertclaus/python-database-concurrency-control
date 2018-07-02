e63230e80f929f91daedbe52021bac8520c46c44
Robert Claus <robertclaus@gmail.com>
2018-07-02 16:37:49 -0500

    See if this helps with freezing

--- policies/PhasedPolicy.py
+++ policies/PhasedPolicy.py
@@ -132,11 +132,12 @@ class PhasedPolicy(AbstractPolicy):
             self.start_next_phase()
             return self.call_phase_admit(True)
 
-        if self.admitted_query_count < (self.current_phase.total_count()*2):
-            return self.call_phase_admit(False)
-
         if self.current_phase.queries and len(self.current_phase.queries) < self.current_phase.min_queries_this_phase():
             self.delay_remaining_queries()
+            return []
+
+        if self.admitted_query_count < (self.current_phase.total_count()*2):
+            return self.call_phase_admit(False)
 
         return []
 

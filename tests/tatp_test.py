ab168a13825d1824f555b5a41978f4690b162b62
Robert Claus <robertclaus@gmail.com>
2018-07-05 10:30:47 -0500

    more data points!

--- tests/tatp_test.py
+++ tests/tatp_test.py
@@ -43,7 +43,7 @@ for dbclient in [MySQLClient]:
         for isolation_level in ['ru-phased','ru', 's']:
             phase_lengths = [100]
             if isolation_level == 'ru-phased':
-                phase_lengths = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 35, 40, 45, 50, 55, 60, 65, 70]
+                phase_lengths = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 24, 26, 28, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200]
 
             for phase_length in phase_lengths:
                 config.QUERIES_TO_INITIALLY_ADMIT = phase_length
